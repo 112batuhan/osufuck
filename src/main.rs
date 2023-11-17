2c05730f6503b47151edfd7bfda8a4a913c89ffc
112batuhan <batuhancakir97@hotmail.com>
2023-11-17 03:25:54 +0300

    feat: adding cmd pause after execution

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::{Parser, ValueEnum};
 
 use osufuck::{debug, execute};
 use rfd::FileDialog;
+use std::io;
 use std::path::PathBuf;
 
 #[derive(ValueEnum, Debug, Clone)]
@@ -43,4 +44,6 @@ fn main() {
         Mode::DEBUG => debug(beatmap_path),
         Mode::EXECUTE => execute(beatmap_path),
     }
+    println!("Press enter to close.");
+    io::stdin().read_line(&mut String::new()).unwrap();
 }
